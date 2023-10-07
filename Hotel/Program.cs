46f49fc4277d08602e82e26a5178982daba10d72
PedroAugustoDev <74946703+PedroAugustoDev@users.noreply.github.com>
2023-10-06 22:53:38 -0300

    colocando métodos

--- Hotel/Program.cs
+++ Hotel/Program.cs
@@ -11,24 +11,32 @@ namespace HotelApp
         {
             Quarto[] quartos = new Quarto[NUMERO_PADRAO_QUARTOS];
 
-            //Lendo a quantidade de novos proprietáritos
             Console.Write("Digite a quantidade de proprietários a ser registrada: ");
             int n = int.Parse(Console.ReadLine());
 
-            for(int i = 0; i < n; i++) {
-                Console.Write("Digite o seu nome: ");
-                string nome = Console.ReadLine();
-                Console.Write($"{nome}, digite o seu email ");
-                string email = Console.ReadLine();  
-                Console.Write($"{nome}, digite o quarto desejado: ");
-                int quartoId = int.Parse(Console.ReadLine()) - 1;
-                Console.WriteLine($"Parabéns {nome}, você está no quarto {quartoId + 1}");
-                quartos[quartoId] = new Quarto(quartoId, nome, email);
+            for (int i = 0; i < n; i++)  AlugarQuarto(quartos);
+            RelatorioDosQuartos(quartos);
+
+        }
+
+        private static void AlugarQuarto(Quarto[] quartos)
+        {
+            Console.Write("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write($"{nome}, digite o seu email: ");
+            string email = Console.ReadLine();
+            Console.Write($"{nome}, digite o quarto desejado: ");
+            int quartoId = int.Parse(Console.ReadLine()) - 1;
+            quartos[quartoId] = new Quarto(quartoId, nome, email);
+            Console.Clear();
+        }
+
+        private static void RelatorioDosQuartos(Quarto[] quartos)
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                if (quartos[i] != null) Console.WriteLine(quartos[i]);
             }
-
-
-            //relatório de quarto utilizados
-            for (int i = 0; i < 10; i++) { if (quartos[i] != null) Console.WriteLine(quartos[i]); }
         }
     }
 }
